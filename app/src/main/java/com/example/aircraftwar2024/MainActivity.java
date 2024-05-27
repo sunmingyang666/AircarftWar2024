ec0a7b865751863d79bcecb11d23c6d9928bc828
sunmingyang666 <113240626+sunmingyang666@users.noreply.github.com>
2024-05-28 00:33:57 +0800

    拨乱反正

--- app/src/main/java/com/example/aircraftwar2024/MainActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/MainActivity.java
@@ -1,20 +1,43 @@
 package com.example.aircraftwar2024;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import com.example.aircraftwar2024.R;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioButton radioButton1,radioButton2;
+    private Button startBotton;
     //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//new11
-    }
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton2 = findViewById(R.id.radioButton2);
+        startBotton = findViewById(R.id.startButton);
 
-    public void offline(){
-        //onCreate(savedInstanceState);
-        setContentView(R.layout.activity_offline);
+        radioButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(radioButton2.isChecked()){
+                    radioButton2.setChecked(false);
+                }
+            }
+        });
 
+        startBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,OfflineActivity.class);
+                boolean isMusicOn = radioButton1.isChecked();
+                intent.putExtra("music",isMusicOn);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/aircraftwar2024/OfflineActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/OfflineActivity.java
@@ -1,13 +1,18 @@
 package com.example.aircraftwar2024;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
-public class OfflineActivity extends AppCompatActivity {
+import com.example.aircraftwar2024.R;
 
+public class OfflineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline);
+        Intent intent = getIntent();
+        boolean isMusicOn = getIntent().getBooleanExtra("music",false);
     }
 }
