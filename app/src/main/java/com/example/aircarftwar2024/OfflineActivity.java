996f6abd804aa650d42c90462750cfe39957505d
sunmingyang666 <113240626+sunmingyang666@users.noreply.github.com>
2024-05-27 17:23:31 +0800

    lab-1

---
+++ app/src/main/java/com/example/aircarftwar2024/OfflineActivity.java
@@ -0,0 +1,13 @@
+package com.example.aircarftwar2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+    }
+}
