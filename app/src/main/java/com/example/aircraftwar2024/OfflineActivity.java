99bf811e2d74ea1601138b70a93cf01f63eefd81
sunmingyang666 <113240626+sunmingyang666@users.noreply.github.com>
2024-05-28 11:21:47 +0800

    拨乱反正

--- app/src/main/java/com/example/aircraftwar2024/OfflineActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/OfflineActivity.java
@@ -4,16 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.aircraftwar2024.R;
+import com.example.aircraftwar2024.activity.GameActivity;
 
 public class OfflineActivity extends AppCompatActivity {
+    private Button easyButton,normalButton,difficultButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline);
         Intent intent = getIntent();
         boolean isMusicOn = getIntent().getBooleanExtra("music",false);
-        //test
+        easyButton=findViewById(R.id.easyButton);
+        normalButton=findViewById(R.id.normalButton);
+        difficultButton=findViewById(R.id.difficultButton);
+
+        easyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OfflineActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        normalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OfflineActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        difficultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OfflineActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
